be3599ff18a7735b9379a68c4c423127548abe01
SuchitKS <155285799+SuchitKS@users.noreply.github.com>
2025-11-09 00:49:47 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,92 +1,52 @@
 import React from 'react';
-
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-
+import ProtectedRoute from './ProtectedRoute'; // Add this import
 import Login from './Login';
-
 import Events from './Events';
-
 import Participants from './Participants';
-
 import Organisers from './Organisers';
-
 import Volunteers from './Volunteers';
-
 import EventForm from './Event_form';
-
 import Registerevent from './Registerevent';
-
 import VolunteerEvents from './Volunteer_events';
-
 import OrganizerTicket from './OrganizerTicket';
-
 import VolunteerTicket from './VolunteerTicket';
-
 import ParticipantTicket from './ParticipantTicket';
-
 import AboutUs from './Aboutus';
-
 import QrCode from './QrCode';
-
 import Scanner from './Scanner';
-
 import ForgotPassword from './ForgotPassword'; 
-
 import ResetPassword from './ResetPassword';   
 
-
-
 function App() {
-
   return (
-
     <Router>
-
       <Routes>
-
+        {/* Public routes */}
         <Route path="/" element={<Login />} />
-
         <Route path="/forgot-password" element={<ForgotPassword />} /> 
-
         <Route path="/reset-password" element={<ResetPassword />} />   
-
-        <Route path="/events" element={<Events />} />
-
-        <Route path="/participants" element={<Participants />} />
-
-        <Route path="/organisers" element={<Organisers />} />
-
-        <Route path="/volunteers" element={<Volunteers />} />
-
-        <Route path="/create-event" element={<EventForm />} />
-
-        <Route path="/register-event" element={<Registerevent />} />
-
-        <Route path="/volunteer-event" element={<VolunteerEvents/>} />
-
-        <Route path="/organiser-ticket" element={<OrganizerTicket/>} />
-
-        <Route path="/participant-ticket" element={<ParticipantTicket/>} />
-
-        <Route path="/volunteer-ticket" element={<VolunteerTicket/>} />
-
         <Route path="/about-us" element={<AboutUs/>} />
-
-        <Route path="/qr" element={<QrCode/>} />
-
-        <Route path="/scanner" element={<Scanner/>} />
-
+        
+        {/* Protected routes */}
+        <Route path="/events" element={<ProtectedRoute><Events /></ProtectedRoute>} />
+        <Route path="/participants" element={<ProtectedRoute><Participants /></ProtectedRoute>} />
+        <Route path="/organisers" element={<ProtectedRoute><Organisers /></ProtectedRoute>} />
+        <Route path="/volunteers" element={<ProtectedRoute><Volunteers /></ProtectedRoute>} />
+        <Route path="/create-event" element={<ProtectedRoute><EventForm /></ProtectedRoute>} />
+        <Route path="/register-event" element={<ProtectedRoute><Registerevent /></ProtectedRoute>} />
+        <Route path="/volunteer-event" element={<ProtectedRoute><VolunteerEvents/></ProtectedRoute>} />
+        <Route path="/organiser-ticket" element={<ProtectedRoute><OrganizerTicket/></ProtectedRoute>} />
+        <Route path="/participant-ticket" element={<ProtectedRoute><ParticipantTicket/></ProtectedRoute>} />
+        <Route path="/volunteer-ticket" element={<ProtectedRoute><VolunteerTicket/></ProtectedRoute>} />
+        <Route path="/qr" element={<ProtectedRoute><QrCode/></ProtectedRoute>} />
+        <Route path="/scanner" element={<ProtectedRoute><Scanner/></ProtectedRoute>} />
+        
+        {/* Catch all */}
         <Route path="*" element={<Navigate to="/" replace />} />
-
       </Routes>
-
     </Router>
-
   );
-
 }
 
-
-
 export default App;
-
